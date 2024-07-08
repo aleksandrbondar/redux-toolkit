7a40699bf81ae5d18c4788c93dad4dd19254811e
aleksandrbondar <aleksandr_bondar@icloud.com>
2024-07-08 12:27:46 +0300

    v1.1

--- src/components/redux/store.ts
+++ src/components/redux/store.ts
@@ -13,6 +13,12 @@ const logger: Middleware = (storeApi) => (next) => (action) => {
   const prevState = storeApi.getState()
   const result = next(action)
   const nextState = storeApi.getState()
+
+  console.groupCollapsed(action)
+  console.log('prev state', prevState)
+  console.log('action', action)
+  console.log('next state', nextState)
+  console.groupEnd()
   return result
 }
 
